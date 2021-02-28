b5a19cd17d939680ec3b51f0df6f92eb247abb02
alondra <anderfrank15@gmail.com>
2021-02-28 03:20:15 -0400

    Se realiza cambio de codigo

--- build/script.js
+++ build/script.js
@@ -1,96 +1,39 @@
-class Task{
-	constructor(name){
-		this.name = name;
-		this.isComplete = false;
-	}
+const taskInput = document.getElementById('taskInput');
+const taskContainer = document.getElementById('taskContainer');
 
-	complete(){
-		this.isComplete = !this.isComplete; 
-	}
-}
+taskInput.addEventListener('keyup',add);
 
-class TaskList{
-	constructor(name){
-		this.name = name;
-		this.tasks = [];
-	}
-
-	addTask(task, element){
-		this.tasks.push(task);
-		this.renderTasks(element);
-	}
-
-	removeTask(i, element){
-		this.tasks.splice(i,1);
-		this.renderTasks(element)
-	}
-
-	renderTasks(element){
-		let tasks = this.tasks.map( task => `
-			<li class="task ${task.isComplete ? "complete" : ""}">
-				<input type="checkbox" class="task_complete-button" ${task.isComplete ? "checked" : ""}>
-				<span>${task.name}</span>
-				<a href="#" class="task_remove-button">X</a>
-			</li>
-		`);
-		element.innerHTML = tasks.reduce( (a,b) => a + b, "")
+function add(argument) {
+	if (argument.key === 'Enter') {
+		const task = document.createElement('DIV');
+		task.classList.add('task')
+		task.innerHTML=`
+					<img>
+					<span>${taskInput.value}</span>
+					<a href='#'>X</a>
+		`;
+		taskContainer.appendChild(task);
+		taskInput.value='';
 	}
 }
 
-// Trabajar con el DOM
+taskContainer.addEventListener('click', checked);
 
-const addTaskElement = document.getElementById('add-task');
-const taskContainerElement = document.getElementById("tasks");
-
-const inbox = new TaskList('inbox')
-
-// Añadir desde el DOM
-
-addTaskElement.addEventListener("keyup", addDOMTask)
-
-
-function addDOMTask(e, list = inbox){
-	// Obterner el texto del input
-	if (e.key === 'Enter') {
-		// Crear la tarea instanciando la clase
-		let task = new Task(this.value);
-		// Añadir la tarea a la lista
-		list.addTask(task, taskContainerElement);
-		// Borra texto del input
-
-		this.value = "";
-	}
-	
-	
-}
-
-//Obtener indice de la tarea actual
-
-function getTaskIndex(e){
-	let taskItem = e.target.parentElement,
-		taskItems = [...taskContainerElement.querySelectorAll('li')];
-	return taskItems.indexOf(taskItem);
-}
-
-// Eliminar tarea desde el DOM
-
-taskContainerElement.addEventListener("click",removeDOMtask)
-
-function removeDOMtask(e, list = inbox){
-	//Detectar que se hizo click en el enlace
-	if (e.target.tagName === "A") {
-		// remover tarea de la lista (Se necesita el indice)
-		list.removeTask(getTaskIndex(e),taskContainerElement)
+function checked(argument) {
+	if (argument.target.tagName === 'SPAN' ) {
+		argument.target.parentElement.classList.toggle('checked');
+		argument.target.parentElement.className === 'task checked' ? 
+		argument.target.previousElementSibling.setAttribute('src','./Imgs/check.png') : 
+		argument.target.previousElementSibling.setAttribute('src','');
 	}
 }
 
-taskContainerElement.addEventListener("click",completeDOMtask)
+taskContainer.addEventListener('click', remove);
 
-function completeDOMtask(e, list = inbox){
-	//Detectar que se hizo click en el enlace
-	if (e.target.tagName === "INPUT") {
-		// remover tarea de la lista (Se necesita el indice)
-		list.tasks[getTaskIndex(e)].complete();
-		e.target.parentElement.classList.toggle("complete");
-	}		
+function remove(argument) {
+	if(argument.target.tagName === 'A'){
+		argument.target.parentElement.setAttribute('id','remove')
+		const taskRemoved = document.getElementById('remove')
+		taskContainer.removeChild(taskRemoved)
+	}
 }
